5db7766b41e2b5754559a86b3fa30a0c8073fcbf
mohammad alkhatib <mohammad.alkhatib@optimumpartners.co>
2022-12-07 18:39:00 +0300

    show case

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { useInfiniteScroll } from 'infinite-scroll-hook'
+import articles from './articles.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const List = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+`
+
+const Item = styled.div`
+  background-color: gold;
+  padding: 12px;
+  border-radius: 8px;
+  font-weight: bold;
+`
+const Loading = styled.p`
+  text-align: center;
+`
+
+const sleep = (timeout) => new Promise((res) => setTimeout(res, timeout))
+
+async function fetchList(lastN = 0, length = 2, timeout = 1500) {
+  await sleep(timeout)
+  return [...articles][lastN]
 }
 
-export default App;
+let counter = 0;
+
+export default function App() {
+  const [list, setList] = useState([articles[0]])
+  const isLastArticle = counter === articles.length-1;
+  const { containerRef, isLoading } = useInfiniteScroll({
+    shouldStop: isLastArticle,
+    offset: '200px',
+    async onLoadMore() {
+      counter++;
+      const res = await fetchList(counter)
+      setList(list.concat(res))
+    },
+  })
+
+  return (
+    <div className="App">
+      <List ref={containerRef}>
+        {list.map((n) => (
+          <Item key={n}>
+            <h1>{n.title}</h1>
+            <p>{n.body}</p>
+          </Item>
+        ))}
+        {isLoading && <Loading>Loading Next Article ...</Loading>}
+        <p>
+        </p>
+      </List>
+    </div>
+  )
+}
